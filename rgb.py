7c5cfad13ad781c58a1bd33a539321cd0850f099
Nina-S-Mathew <99273767+Nina-S-Mathew@users.noreply.github.com>
2023-04-27 15:27:44 +0000

    eddit stuff

--- rgb.py
+++ rgb.py
@@ -9,9 +9,9 @@ def main() -> None:
 
 
     # process
-    for r_value in range(0, 255):
-        for g_value in range(0, 255):
-            for b_value in range(0, 255):
+    for r_value in range(0, 256):
+        for g_value in range(0, 256):
+            for b_value in range(0, 256):
                 print("RGB({0}, {1}, {2})".format(r_value, g_value, b_value))
     print("\nDone.")
 
